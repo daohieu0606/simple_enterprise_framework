d0a66aedbb74a540048025c9cdf3d2410303f71b
nguyễn văn hải <nvhaik18@gmail.com>
2022-01-01 06:43:17 +0700

    Add Helper

---
+++ SimpleEnterpriseFramework/HelperLibrary/TableHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace HelperLibrary
+{
+    public class TableHelper
+    {
+        public static DataTable addColumn(DataTable table, string name, string type)
+        {
+            DataColumn column;
+            column = new DataColumn();
+            column.DataType = System.Type.GetType(type);
+            column.ColumnName = name;
+            table.Columns.Add(column);
+            return table;
+        }
+        public static void printDataRow(DataRow dr)
+        {
+            foreach (var item in dr.ItemArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
